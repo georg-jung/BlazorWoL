41c3d6a29a66b8fe9e540b6aeb54251b994694d4
Georg Jung <gjung@gimas.net>
2019-12-12 17:05:41 +0200

    Add pinging to host list

---
+++ WoL/Models/ViewModels/HostViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoL.Extensions;
+
+namespace WoL.Models.ViewModels
+{
+    public class HostViewModel
+    {
+        public HostViewModel()
+        {
+        }
+
+        public HostViewModel(Host host)
+        {
+            Id = host.Id;
+            Hostname = host.Hostname;
+            Caption = host.Caption;
+            MacAddress = host.GetMacString();
+            Status = string.IsNullOrEmpty(Hostname) ? HostStatus.NoHostname : HostStatus.Loading;
+        }
+
+        public int Id { get; set; }
+
+        public string Hostname { get; set; }
+
+        public string Caption { get; set; }
+
+        public string MacAddress { get; set; }
+
+        public HostStatus Status { get; set; }
+
+        public enum HostStatus
+        {
+            Loading,
+            Online,
+            Unreachable,
+            HostnameInvalid,
+            NoHostname
+        }
+    }
+}
