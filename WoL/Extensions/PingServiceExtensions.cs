f7dad76eedae427b10d8a83db2eaaed725054f1a
Georg Jung <gjung@gimas.net>
2019-12-12 18:13:35 +0200

    Add host status to wake page

---
+++ WoL/Extensions/PingServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoL.Models.ViewModels;
+using WoL.Services;
+
+namespace WoL.Extensions
+{
+    public static class PingServiceExtensions
+    {
+        public static HostViewModel.HostStatus ToHostStatus(this IPingService.PingResult result)
+        {
+            return result switch
+            {
+                IPingService.PingResult.Success => HostViewModel.HostStatus.Online,
+                IPingService.PingResult.Unreachable => HostViewModel.HostStatus.Unreachable,
+                IPingService.PingResult.HostNotFound => HostViewModel.HostStatus.HostnameInvalid,
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
