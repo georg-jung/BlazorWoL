3d8863e6c6a7909509cdaa43d09dcb4328673fd6
Georg Jung <gjung@gimas.net>
2019-12-12 03:16:43 +0200

    Add list, add, delete and wake

---
+++ WoL/Models/ViewModels/AddHostViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WoL.Extensions;
+
+namespace WoL.Models.ViewModels
+{
+    public class AddHostViewModel : IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Hostname { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Caption { get; set; }
+
+        [StringLength(17, MinimumLength = 17)]
+        public string MacAddress { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(MacAddress) && string.IsNullOrWhiteSpace(Hostname))
+                yield return new ValidationResult("You must enter either a hostname or a mac address.", new[] { nameof(Hostname) });
+            if (!string.IsNullOrWhiteSpace(MacAddress) && !string.IsNullOrWhiteSpace(Hostname))
+                yield return new ValidationResult("You must enter either a hostname or a mac address, but not both.", new[] { nameof(Hostname) });
+            if (!string.IsNullOrEmpty(MacAddress) && !MacAddress.TryParseMacAddress(out _))
+                yield return new ValidationResult("The entered mac address is not valid.", new[] { nameof(MacAddress) });
+        }
+    }
+}
